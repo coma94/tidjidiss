f018f0bf632ff9ea212f43190479b8e217947681
Eugenia <yarutoua@gmail.com>
2016-03-14 16:29:12 +0100

    Added: data structures, skeleton for parsing

---
+++ data_structures.py
@@ -0,0 +1,27 @@
+"""."""
+
+
+class Schema:
+    # relations
+    pass
+
+
+class Relation:
+    # name
+    # fields = [Field1, Field2, ...]
+    pass
+
+
+class RelationInstance:
+    # relation = Relation()
+    # variables = [VarName1, VarName2, ..] position is important!!!
+    pass
+
+
+class Mapping:
+    # rhs - right hand side of rule
+    # lhs - left hand side of rule
+    # rhs = [RelationInstance1, RelationInstance2, ...] - conjunction of these 
+    # relations
+    # lhs = same
+    pass
---
+++ parser.py
@@ -0,0 +1,22 @@
+"""."""
+import ply.yacc as yacc
+
+
+def p_source():
+    """."""
+    pass
+
+
+def p_target():
+    """."""
+    pass
+
+
+def p_mapping():
+    """."""
+    pass
+
+
+def p_relation():
+    """."""
+    pass
