20c0f8a5d1d5162ef7b3f6ad94a9f02a072d3f49
Eugenia <yarutoua@gmail.com>
2016-03-15 23:14:37 +0100

    Added: skolemization of rhs of tgds

---
+++ skolemizer.py
@@ -0,0 +1,31 @@
+"""."""
+from data_structures import SkolemizedMapping
+from data_structures import SkolemTerm
+from data_structures import RelationInstance
+
+
+def skolemize(mappings):
+    """Take list of mappings and return the list of scholemized mappings."""
+    scholemized_mappings = []
+
+    for i, mapping in enumerate(mappings):
+        skol_lhs = mapping.lhs
+        skol_rhs = []
+        universal_variables = set()
+        for instance in mapping.lhs:
+            universal_variables.update(instance.variables)
+        for instance in mapping.rhs:
+            skol_variables = []
+            for var in instance.variables:
+                if var not in universal_variables:
+                    # TODO: now we make skolem term depend on all universal
+                    # variables but later change to depend only ones from rhs
+                    # ORDER OF ARGUMENTS IS NOT PRESERVED
+                    skol_term = SkolemTerm("m%d" % (i+1), var, universal_variables)
+                    skol_variables.append(skol_term)
+                else:
+                    skol_variables.append(var)
+            skol_rhs.append(
+                RelationInstance(instance.relation, skol_variables))
+        scholemized_mappings.append(SkolemizedMapping(skol_lhs, skol_rhs))
+    return scholemized_mappings
