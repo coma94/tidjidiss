1766616a2d6b233e6d3a8ea57c743b18dcda38cb
Eugenia <yarutoua@gmail.com>
2016-03-15 23:14:01 +0100

    Modified: implemented Skolem data structures, doc strings added

--- data_structures.py
+++ data_structures.py
@@ -1,46 +1,72 @@
-"""."""
+"""Definition all data types needed for our schema mapping purposes."""
 
 
 class Schema(list):
-    # relations = [Relation1, Relation2, ..]
+    """Datastructure of shema definition."""
+
     def __init__(self, *args):
+        """Input: list of instances of relation [Rel1, Rel2, ..]."""
         list.__init__(self, *args)
 
 
 class Relation:
-    # name
-    # fields = [Field1, Field2, ...]
+    """Datastructure of relation."""
+
     def __init__(self, name, attributes):
+        """Input name of relation and fields (attributes)."""
         self.name = name
         self.fields = attributes
 
 
 class RelationInstance:
-    # relation = Relation()
-    # variables = [VarName1, VarName2, ..] position is important!!!
+    """Datastructure of relation instance (used in mappings)."""
+
     def __init__(self, relation, variables):
+        """Initialize the relation instance.
+
+        Input arguments
+        - relation instance of Relation()
+        - list of variables [VarName1, VarName2, ..]
+        and their position is important
+        """
         self.relation = relation
         self.variables = variables
 
 
 class Mapping:
-    # rhs - right hand side of rule
-    # lhs - left hand side of rule
-    # rhs = [RelationInstance1, RelationInstance2, ...] - conjunction of these
-    # relations
-    # lhs = same
+    """Datastructure of mapping."""
+
     def __init__(self, lhs, rhs):
+        """Initialize the mapping.
+
+        rhs - right hand side of rule
+        lhs - left hand side of rule, where
+        both lhs and rhs are lists of RelationInstance() objects.
+        We interpret the list as conjunction of these relations.
+        """
         self.lhs = lhs
         self.rhs = rhs
 
 
 class SkolemTerm:
-    # mapping_name = ..
-    # variable = ..
-    # arguments = [VarName1, Varname2, ..]
-    pass
+    """Datastructure of skolem term."""
+
+    def __init__(self, name, variable, arguments):
+        """Initialize skolem term.
+
+        Takes on input mapping_name, variable that is skolemized
+        arguments = [VarName1, Varname2, ..] list of variables
+        on which skolem term depends
+        """
+        self.mapping_name = name
+        self.variable = variable
+        self.arguments = arguments
 
 
 class SkolemizedMapping(Mapping):
-    # it can contain in rhs and lhs both variables and SkolemTerms
-    pass
+    """Datastructure of skolemized mapping."""
+
+    def __init__(self, lhs, rhs):
+        """The same as mapping but can contain as variables skolm terms."""
+        self.lhs = lhs
+        self.rhs = rhs
