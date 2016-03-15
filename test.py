9ca36518858c9f9dae6f0b66f70ed16c290027b7
Eugenia <yarutoua@gmail.com>
2016-03-15 23:15:52 +0100

    Added: script to test integrity of all together

---
+++ test.py
@@ -0,0 +1,32 @@
+"""Test implemeted scripts."""
+from parser import parse
+# from parser import source
+# from parser import target
+from parser import tgds
+from skolemizer import skolemize
+
+
+if __name__ == '__main__':
+    file = "examples/example-input-file.txt"
+    with open(file, 'r+') as f:
+        data = f.read()
+        parse(data)
+        skolemized_tgds = skolemize(tgds)
+        for i, sk_tgd in enumerate(skolemized_tgds):
+            print("m%d:" % (i + 1))
+            print("Left hand side: ")
+            for instance in sk_tgd.lhs:
+                print(instance.relation.name)
+                print(instance.variables)
+            print("Right hand side:")
+            for instance in sk_tgd.rhs:
+                print(instance.relation.name)
+                for var in instance.variables:
+                    if type(var) == str:
+                        print(var)
+                    else:
+                        print("f_%s,%s(%s)" % (
+                            var.mapping_name,
+                            var.variable,
+                            ",".join(var.arguments)))
+            print("\n")
